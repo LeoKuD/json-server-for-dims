12304afd40a66225e8ca6b93ac4e12d1ce6992f9
LeoKuD <then7w@gmail.com>
2022-02-23 19:37:42 +0300

    feat: add main data

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const items = require('./data/items');
+const tags = require('./data/tags');
+const brands = require('./data/brands');
+const itemTypes = require('./data/itemTypes');
+const users = require('./data/users');
+const tasks = require('./data/tasks');
 
 module.exports = {
   items,
   tags,
   brands,
-  itemTypes
-}
+  itemTypes,
+  users,
+  tasks,
+};
