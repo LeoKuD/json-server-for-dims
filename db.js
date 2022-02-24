0d88af23173ba05c258b0e8124101fa1ea7a6648
LeoKuD <then7w@gmail.com>
2022-02-24 09:11:12 +0300

    fix: add only main data

--- db.js
+++ db.js
@@ -1,15 +1,8 @@
-const items = require('./data/items');
-const tags = require('./data/tags');
-const brands = require('./data/brands');
-const itemTypes = require('./data/itemTypes');
+
 const users = require('./data/users');
 const tasks = require('./data/tasks');
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes,
   users,
   tasks,
 };
