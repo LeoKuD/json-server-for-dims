c2cea2c2825ce90ef803d34b5db340b8497c9bb4
LeoKuD <then7w@gmail.com>
2022-02-26 15:40:55 +0300

    fix: fix structure DB

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 
 const users = require('./data/users');
 const tasks = require('./data/tasks');
+const subTasks = require('./data/subTasks');
+const tracks = require('./data/tracks');
 
 module.exports = {
   users,
   tasks,
+  subTasks,
+  tracks,
 };
